4f74296818fb9d126284e32dfbe09c13f9bcfdab
Brodie Peberdy <brodie.peberdy@gmail.com>
2021-05-14 13:35:32 +0100

    Application now displays the solution, some styling tidied up.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# sudoku-solver
+# Sudoku Solver
 Java application for solving Sudoku puzzles.
+## Usage
+Using the command line navigate to src/main/java and run the following command:
+```bash
+java --module-path ./lib/ --add-modules=javafx.controls SudokuSolver
+```
+## Example
+<p>
+    <img src="src/main/resources/screenshot1.png"></img>
+</p>
--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -28,22 +28,34 @@ public class SudokuSolver extends Application {
     gridPane.setVgap(5);
     gridPane.setHgap(5);
     gridPane.setAlignment(Pos.CENTER);
+    gridPane.setStyle("-fx-background-color: LIGHTCYAN;");
 
     GridPane sudokuPane = new GridPane();
     sudokuPane.setStyle("-fx-background-color: WHITE;");
+    sudokuPane.setStyle("-fx-background-color: LIGHTCYAN;");
     sudokuPane.setMinSize(450, 450);
-    gridPane.add(sudokuPane, 0, 2);
+    gridPane.add(sudokuPane, 0, 1);
+
+    GridPane solutionPane = new GridPane();
+    solutionPane.setStyle("-fx-background-color: WHITE;");
+    solutionPane.setStyle("-fx-background-color: LIGHTCYAN;");
+    solutionPane.setMinSize(450, 450);
+    gridPane.add(solutionPane, 1, 1);
 
     Text titleText = new Text("Sudoku Solver");
     titleText.setStyle("-fx-font: normal bolder 40px 'sans-serif';");
     gridPane.add(titleText, 0, 0);
 
-    Text descText = new Text("This application uses a backtracking algorithm to solve an entered Sudoku puzzle.");
-    descText.setStyle("-fx-font: normal 20px 'sans-serif';");
-    gridPane.add(descText, 0, 1);
+    Text descText1 = new Text("Puzzle to be Solved");
+    descText1.setStyle("-fx-font: normal 20px 'sans-serif';");
+    gridPane.add(descText1, 0, 2);
+
+    Text descText2 = new Text("Solved Puzzle");
+    descText2.setStyle("-fx-font: normal 20px 'sans-serif';");
+    gridPane.add(descText2, 1, 2);
 
     Button solvePuzzle = new Button("Solve Sudoku Puzzle");
-    solvePuzzle.setStyle("-fx-background-color: ROYALBLUE; -fx-text-fill: white;");
+    solvePuzzle.setStyle("-fx-background-color: DARKBLUE; -fx-text-fill: white;");
     solvePuzzle.setOnAction(new EventHandler<ActionEvent>(){
       @Override
       public void handle(final ActionEvent AE){
@@ -62,19 +74,41 @@ public class SudokuSolver extends Application {
 
         solutionFinder = new sudoku.SolutionFinder(nums);
         int[][] solution = solutionFinder.findSolution();
+
         if (solution != null){
           for (int y = 0; y < 9; y++){
             for (int x = 0; x < 9; x++){
-              System.out.print(solution[y][x]);
+              TextField square = new TextField();
+              square.setMinSize(50,50);
+              square.setMaxSize(50,50);
+              square.setAlignment(Pos.CENTER);
+              square.setTextFormatter(new TextFormatter<>(change -> {
+                if (change.getControlNewText().matches("[1-9]?")) {
+                  return change;
+                }
+                return null;
+              }));
+              square.setStyle("-fx-border-color: top right bottom left; -fx-border-color: transparent transparent transparent transparent; -fx-focus-color: PURPLE;");
+              square.setText(String.valueOf(solution[y][x]));
+              square.setEditable(false);
+              if (x % 3 == 0 && x != 0 && y % 3 == 0 && y != 0){
+                square.setStyle("-fx-border-color: top right bottom left; -fx-border-color: black transparent transparent black; -fx-focus-color: PURPLE;");
+              }
+              else if (x % 3 == 0 && x != 0){
+                square.setStyle("-fx-border-color: top right bottom left; -fx-border-color: transparent transparent transparent black; -fx-focus-color: PURPLE;");
+              }
+              else if (y % 3 == 0 && y != 0){
+                square.setStyle("-fx-border-color: top right bottom left; -fx-border-color: black transparent transparent transparent; -fx-focus-color: PURPLE;");
+              }
+              solutionPane.add(square, x, y);
             }
-            System.out.print("\n");
           }
-          System.out.print("\n");
-          System.out.print("\n");
+          gridPane.getChildren().remove(solutionPane);
+          gridPane.add(solutionPane, 1, 1);
         }
       }
     });
-    gridPane.add(solvePuzzle, 0, 3);
+    gridPane.add(solvePuzzle, 1, 0);
 
     for (int y = 0; y < 9; y++){
       for (int x = 0; x < 9; x++){
